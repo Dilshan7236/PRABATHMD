6b1fe3572c540957c5f79d45d3036d75e12af3ea
Dilshan7236 <ashinsadilshan41@gmail.com>
2024-12-20 01:23:02 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "94742876482"
+const SESSION_ID = "PRABATH-MD~F7lDQa5C#Ss2LS56Ih1vd-r8qIcNULo_hnSaLmO0pHH-UmJhJy48"
 
 
 module.exports = {
